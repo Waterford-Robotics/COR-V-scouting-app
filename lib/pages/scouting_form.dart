4d24e2ee757c23f9490ee033c804a5e81b347989
Cooper Benedict <cooperbenedict@students.waterfordschool.org>
2024-02-03 11:44:39 -0700

    Add Firestore data upload functionality

--- lib/pages/scouting_form.dart
+++ lib/pages/scouting_form.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//add test input data to firestore
+Future<void> uploadingData(String _testInput) async {
+  await FirebaseFirestore.instance
+      .collection("test")
+      .add({'test_input': _testInput});
+}
 
 class ScoutingPage extends StatefulWidget {
   const ScoutingPage({Key? key}) : super(key: key);
@@ -26,28 +34,41 @@ class _ScoutingPageState extends State<ScoutingPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: TextField(
-          controller: myController,
+        child: Column(
+          children: [
+            TextField(
+              controller: myController,
+            ),
+            //add a confirmation prompt to this and make it clear data
+            ElevatedButton(
+              onPressed: () {
+                uploadingData(myController.text);
+              },
+              child: const Text('Upload Data'),
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        // When the user presses the button, show an alert dialog containing
-        // the text that the user has entered into the text field.
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                // Retrieve the text the that user has entered by using the
-                // TextEditingController.
-                content: Text(myController.text),
-              );
-            },
-          );
-        },
-        tooltip: 'Show me the value!',
-        child: const Icon(Icons.text_fields),
-      ),
-    ); // Add the missing closing parenthesis and closing brace here.
+    );
   }
 }
+        
+
+      // floatingActionButton: FloatingActionButton(
+      //   // When the user presses the button, show an alert dialog containing
+      //   // the text that the user has entered into the text field.
+      //   onPressed: () {
+      //     showDialog(
+      //       context: context,
+      //       builder: (context) {
+      //         return AlertDialog(
+      //           // Retrieve the text the that user has entered by using the
+      //           // TextEditingController.
+      //           content: Text(myController.text),
+      //         );
+      //       },
+      //     );
+      //   },
+      //   tooltip: 'Show me the value!',
+      //   child: const Icon(Icons.text_fields),
+      // ),
